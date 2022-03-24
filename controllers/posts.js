7ea7a8a0b8e2e0837b68494fa9ec6cd7767e6e69
mattjacobs93 <matthewjacobs73@yahoo.com>
2022-03-24 15:48:37 -0700

    bug in create post function

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,41 @@
 import { Post } from '../models/post.js'
+import { v2 as cloudinary } from 'cloudinary'
 
-function create (req, res) {
-  console.log(req.body)
-
+function create(req, res) {
+  console.log('req:' ,req.body)
+  //req.body.owner = req.user.profile
+  //console.log(req.body)
+  if (req.body.images === 'undefined' || !req.files['images']) {
+    delete req.body['images']
+    Post.create(req.body)
+    .then(post => {
+      post.populate('author')
+      .then(populatedPost => {
+        res.status(201).json(populatedPost)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  } else {
+    const imageFile = req.files.images.path
+    cloudinary.uploader.upload(imageFile, {tags: `${req.body.name}`})
+    .then(image => {
+      req.body.photo = image.url
+      Post.create(req.body)
+      .then(post => {
+        post.populate('author')
+        .then(populatedPost => {
+          res.status(201).json(populatedPost)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+    })
+  }
 }
 
 export { create }
